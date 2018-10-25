700f8bff78b3880c4b594918c5cea9a41c73fff6
looooo <sppedflyer@gmail.com>
2018-10-25 15:16:44 +0200

    update recipe

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,14 +1,13 @@
 mkdir -p build
 cd build
 
-export LIBRARY_PATH=$PREFIX/lib
-
 cmake -G "Ninja" \
       -D CMAKE_INSTALL_PREFIX=$PREFIX \
       -D CMAKE_PREFIX_PATH=$PREFIX \
       -D SIMAGE_RUNTIME_LINKING=ON \
       -D CMAKE_INSTALL_LIBDIR=$PREFIX/lib \
       -D CMAKE_BUILD_TYPE="Release" \
+      -D CMAKE_CXX_FLAGS="-I/usr/include -L/usr/lib64" \
       ..
 
 ninja install
