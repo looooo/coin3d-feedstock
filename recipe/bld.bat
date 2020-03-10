45c987c9e9e54b767fd75ebddbe92b42ef884145
lorenz <looooo@users.noreply.github.com>
2020-03-10 15:05:41 +0100

    windows: add superglu (#17)
    * win: add superglu for windows
    * linux: remove libxmu, don't install libGLU

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,22 @@
+cd superglue
+mkdir build
+cd build
+
+
+cmake -G "Ninja" ^
+      -D CMAKE_BUILD_TYPE:STRING="Release" ^
+      -D CMAKE_PREFIX_PATH:FILEPATH="%LIBRARY_PREFIX%" ^
+      -D CMAKE_INSTALL_PREFIX:FILEPATH="%LIBRARY_PREFIX%" ^
+      -D CMAKE_INSTALL_LIBDIR:FILEPATH="%LIBRARY_PREFIX%\lib" ^
+      ..
+
+if errorlevel 1 exit 1
+ninja install
+if errorlevel 1 exit 1
+
+cd ..
+cd ..
+
 mkdir build
 cd build
 
@@ -10,6 +29,7 @@ cmake -G "Ninja" ^
       -D COIN_BUILD_DOCUMENTATION:BOOL=OFF ^
       -D COIN_BUILD_TESTS:BOOL=OFF ^
       -D COIN_BUILD_DOCUMENTATION:BOOL=OFF ^
+      -D USE_SUPERGLU:BOOL=ON ^
       ..
 
 if errorlevel 1 exit 1
